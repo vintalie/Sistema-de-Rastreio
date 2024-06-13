3702af22776a95261c624f7cb23f9a8c2a6407dd
vintalie <marioluizsan@gmail.com>
2024-06-13 18:30:53 -0300

    att forms

--- app/consulta/forms.py
+++ app/consulta/forms.py
@@ -6,6 +6,7 @@ class CadastroForm(forms.ModelForm):
     class Meta:
         model = Pedidos
         fields = (
+            'id',
             'nome_cliente',
             'status',
             'prazo', 'cidade', 'estado',
